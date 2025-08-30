bb7ffdc5468c2af629ff377d31f9b6ada8dc6a8c
julesjung <94782485+julesjung@users.noreply.github.com>
2025-08-30 20:40:12 +0200

    fix: seperate overlapping instructions

--- src/assembler/architecture.asm
+++ src/assembler/architecture.asm
@@ -49,36 +49,36 @@
     ADDI {rd: register}, {imm: i8} => le(0x2 @ rd @ imm @ 0b0)
     SUBI {rd: register}, {imm: i8} => le(0x2 @ rd @ imm @ 0b1)
 
-    CMPL {ra: register}, {rb: register} => le(0x3 @ ra @ rb @ 0b000000)
-    CMPH {ra: register}, {rb: register} => le(0x3 @ ra @ rb @ 0b000001)
-    CMP {ra: register}, {rb: register} => le(0x3 @ ra @ rb @ 0b000010)
+    CMP {ra: register}, {rb: register} => le(0x3 @ ra @ rb @ 0b000000)
+    CMPL {ra: register}, {rb: register} => le(0x3 @ ra @ rb @ 0b000010)
+    CMPH {ra: register}, {rb: register} => le(0x3 @ ra @ rb @ 0b000011)
+    
+    CMPL {rs: register}, {imm: i8} => le(0x4 @ ra @ imm @ 0b0)
+    CMPH {rs: register}, {imm: i8} => le(0x4 @ ra @ imm @ 0b1)
 
-    CMPL {ra: register}, {imm: i8} => le(0x3 @ ra @ imm @ 0b0)
-    CMPH {ra: register}, {imm: i8} => le(0x3 @ ra @ imm @ 0b1)
+    MOVL {rd: register}, {imm: i8} => le(0x5 @ rd @ imm @ 0b0)
+    MOVH {rd: register}, {imm: i8} => le(0x5 @ rd @ imm @ 0b1)
 
-    MOVL {rd: register}, {imm: i8} => le(0x4 @ rd @ imm @ 0b0)
-    MOVH {rd: register}, {imm: i8} => le(0x4 @ rd @ imm @ 0b1)
+    LDW {rd: register}, [{rs: register}] => le(0x6 @ rd @ rs @ 0b000000)
+    LDL {rd: register}, [{rs: register}] => le(0x6 @ rd @ rs @ 0b000010)
+    LDH {rd: register}, [{rs: register}] => le(0x6 @ rd @ rs @ 0b000011)
+    
+    STW {rs: register}, [{rd: register}] => le(0x7 @ rs @ rd @ 0b000000)
+    STL {rs: register}, [{rd: register}] => le(0x7 @ rs @ rd @ 0b000010)
+    STH {rs: register}, [{rd: register}] => le(0x7 @ rs @ rd @ 0b000011)
 
-    LDL {rd: register}, [{rs: register}] => le(0x5 @ rd @ rs @ 0b000000)
-    LDH {rd: register}, [{rs: register}] => le(0x5 @ rd @ rs @ 0b000001)
-    LDW {rd: register}, [{rs: register}] => le(0x5 @ rd @ rs @ 0b000010)
+    JMP {offset: off12} => le(0x8 @ offset)
 
-    STL {rs: register}, [{rd: register}] => le(0x6 @ rs @ rd @ 0b000000)
-    STH {rs: register}, [{rd: register}] => le(0x6 @ rs @ rd @ 0b000001)
-    STW {rs: register}, [{rd: register}] => le(0x6 @ rs @ rd @ 0b000010)
+    JMP [{rs: register}] => le(0x9 @ rs @ 0b00000)
 
-    JMP {offset: off12} => le(0x7 @ offset)
-
-    JMP [{rs: register}] => le(0x8 @ rs @ 0b00000)
-
-    BC  {offset: off9} => le(0x9 @ offset @ 0b000)
-    BNC {offset: off9} => le(0x9 @ offset @ 0b001)
-    BO  {offset: off9} => le(0x9 @ offset @ 0b010)
-    BNO {offset: off9} => le(0x9 @ offset @ 0b011)
-    BZ  {offset: off9} => le(0x9 @ offset @ 0b100)
-    BNZ {offset: off9} => le(0x9 @ offset @ 0b101)
-    BS  {offset: off9} => le(0x9 @ offset @ 0b110)
-    BNS {offset: off9} => le(0x9 @ offset @ 0b111)
+    BC  {offset: off9} => le(0xa @ offset @ 0b000)
+    BNC {offset: off9} => le(0xa @ offset @ 0b001)
+    BO  {offset: off9} => le(0xa @ offset @ 0b010)
+    BNO {offset: off9} => le(0xa @ offset @ 0b011)
+    BZ  {offset: off9} => le(0xa @ offset @ 0b100)
+    BNZ {offset: off9} => le(0xa @ offset @ 0b101)
+    BS  {offset: off9} => le(0xa @ offset @ 0b110)
+    BNS {offset: off9} => le(0xa @ offset @ 0b111)
 
     HLT => le(0xf000)
 }
